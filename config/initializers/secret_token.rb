f60ae598b7728470c95ef915612184a96e07ab63
David Anderson <eymiha@gmail.com>
2011-08-26 20:33:08 -0400

    initial 3.0.10 rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetracker::Application.config.secret_token = 'cac2962d058af120ec9a86d8177fc901b6303f9ee9f13f452a57cba67df8f8158d6ac0a5e73206dc3de30fc74e651ab1388438b4a57eccaae6393e22f0e34457'
